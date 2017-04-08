5e565d44b1bdad30019a91ed2585822d0aa3adf5
Jim Shaw <jimxshaw@gmail.com>
2017-04-08 17:08:00 -0500

    In IdentityModel, added DbSet properties and overrode OnModelCreating method.

--- MasterDetail/MasterDetail/Models/IdentityModels.cs
+++ MasterDetail/MasterDetail/Models/IdentityModels.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using MasterDetail.DataLayer;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -20,6 +21,14 @@ namespace MasterDetail.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+        public DbSet<Labor> Labors { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<ServiceItem> ServiceItems { get; set; }
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -28,6 +37,19 @@ namespace MasterDetail.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CategoryConfiguration());
+            modelBuilder.Configurations.Add(new CustomerConfiguration());
+            modelBuilder.Configurations.Add(new InventoryItemConfiguration());
+            modelBuilder.Configurations.Add(new LaborConfiguration());
+            modelBuilder.Configurations.Add(new PartConfiguration());
+            modelBuilder.Configurations.Add(new ServiceItemConfiguration());
+            modelBuilder.Configurations.Add(new WorkOrderConfiguration());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
