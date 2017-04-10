f66ae140889301212fe143b85ff3b31aa1c3b473
Jim Shaw <jimxshaw@gmail.com>
2017-04-09 20:49:12 -0500

    Fixed up the Edit get and post actions in ApplicationRolesController.

--- MasterDetail/MasterDetail/Controllers/ApplicationRolesController.cs
+++ MasterDetail/MasterDetail/Controllers/ApplicationRolesController.cs
@@ -101,36 +101,55 @@ namespace MasterDetail.Controllers
             return View(applicationRole);
         }
 
-        //// GET: ApplicationRoles/Edit/5
-        //public async Task<ActionResult> Edit(string id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    ApplicationRole applicationRole = await db.IdentityRoles.FindAsync(id);
-        //    if (applicationRole == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(applicationRole);
-        //}
+        // GET: ApplicationRoles/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationRole applicationRole = await RoleManager.FindByIdAsync(id);
+            if (applicationRole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationRole);
+        }
 
-        //// POST: ApplicationRoles/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] ApplicationRole applicationRole)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(applicationRole).State = EntityState.Modified;
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(applicationRole);
-        //}
+        // POST: ApplicationRoles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] ApplicationRole applicationRole)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationRole retrievedApplicationRole = await RoleManager.FindByIdAsync(applicationRole.Id);
+                string originalName = retrievedApplicationRole.Name;
+
+                if (originalName == "Admin" && applicationRole.Name != "Admin")
+                {
+                    ModelState.AddModelError("", "You cannot change the name of the Admin role.");
+
+                    return View(applicationRole);
+                }
+
+                if (originalName != "Admin" && applicationRole.Name == "Admin")
+                {
+                    ModelState.AddModelError("", "You cannot change the name of a role to Admin.");
+
+                    return View(applicationRole);
+                }
+
+                retrievedApplicationRole.Name = applicationRole.Name;
+
+                await RoleManager.UpdateAsync(retrievedApplicationRole);
+
+                return RedirectToAction("Index");
+            }
+            return View(applicationRole);
+        }
 
         //// GET: ApplicationRoles/Delete/5
         //public async Task<ActionResult> Delete(string id)
