aa3af9d5e16aab2f0d507cf2c7d0a6541dbb819f
Jim Shaw <jimxshaw@gmail.com>
2017-04-12 20:08:35 -0500

    Updated Category model and config classes. Cleaned up Configuration.cs inside Migrations folder.

--- MasterDetail/MasterDetail/DataLayer/CategoryConfiguration.cs
+++ MasterDetail/MasterDetail/DataLayer/CategoryConfiguration.cs
@@ -13,11 +13,24 @@ namespace MasterDetail.DataLayer
     {
         public CategoryConfiguration()
         {
+            Property(c => c.Id).HasColumnName("CategoryId");
+
             Property(c => c.CategoryName)
                 .HasMaxLength(20)
                 .IsRequired()
                 .HasColumnAnnotation("Index",
                     new IndexAnnotation(new IndexAttribute("AK_Category_CategoryName") { IsUnique = true }));
+
+            // A Category has an optional relationship with the Parent, which can have
+            // many Children related to it, through a foreign key named ParentCategoryId
+            // and the relationship will not cascade delete from Parent to Children.
+            // If we attempt to delete a Category that's parent to one or more children,
+            // the db don't automatically delete the children along with the parent. It
+            // will instead throw an exception.
+            HasOptional(c => c.Parent)
+                .WithMany(c => c.Children)
+                .HasForeignKey(c => c.ParentCategoryId)
+                .WillCascadeOnDelete(false);
         }
     }
 }
