e98355ae17fdaca633ed14cfb1985538ccf1c913
Jim Shaw <jimxshaw@gmail.com>
2017-04-15 13:52:39 -0500

    Added data annotations to ServiceItem model class.

--- MasterDetail/MasterDetail/Models/ServiceItem.cs
+++ MasterDetail/MasterDetail/Models/ServiceItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,20 @@ namespace MasterDetail.Models
     public class ServiceItem
     {
         public int ServiceItemId { get; set; }
+
+        [Required(ErrorMessage = "You must enter an Item Code.")]
+        [StringLength(15, ErrorMessage = "The Item Code must be 15 characters or shorter.")]
+        [Display(Name = "Item Code")]
         public string ServiceItemCode { get; set; }
+
+        [Required(ErrorMessage = "You must enter a Name.")]
+        [StringLength(80, ErrorMessage = "The Item Name must be 80 characters or shorter.")]
+        [Display(Name = "Name")]
         public string ServiceItemName { get; set; }
+
+        // The min and max values for Decimal must be hardcoded in string.
+        [Range(typeof(decimal), "0", "7922816251464337593543950335")]
+        [Display(Name = "Rate")]
         public decimal Rate { get; set; }
     }
 }
