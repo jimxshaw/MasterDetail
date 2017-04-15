f8178d505510f25de1d639564c6708241c0df5a9
Jim Shaw <jimxshaw@gmail.com>
2017-04-15 11:45:15 -0500

    Updated Delete action to enforce rule that a category cannot be deleted if it has child categories.

--- MasterDetail/MasterDetail/Controllers/CategoriesController.cs
+++ MasterDetail/MasterDetail/Controllers/CategoriesController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
@@ -161,9 +162,25 @@ namespace MasterDetail.Controllers
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
             Category category = await _applicationDbContext.Categories.FindAsync(id);
-            _applicationDbContext.Categories.Remove(category);
-            await _applicationDbContext.SaveChangesAsync();
-            return RedirectToAction("Index");
+
+            try
+            {
+                _applicationDbContext.Categories.Remove(category);
+                await _applicationDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "You attempted to delete a category that has child categories.");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+
+            }
+
+            return View("Delete", category);
         }
 
         protected override void Dispose(bool disposing)
