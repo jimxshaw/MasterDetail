0635f15ec9c2ed40d8da0ad319cc10a80107a1d4
Jim Shaw <jimxshaw@gmail.com>
2017-04-16 11:06:44 -0500

    Implemented sorting in Index action of InventoryItems controller.

--- MasterDetail/MasterDetail/Controllers/InventoryItemsController.cs
+++ MasterDetail/MasterDetail/Controllers/InventoryItemsController.cs
@@ -16,38 +16,85 @@ namespace MasterDetail.Controllers
     {
         private ApplicationDbContext _applicationDbContext = new ApplicationDbContext();
 
-        // GET: InventoryItems
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(string sort)
         {
+            // If the sort string is null or empty then CategorySort is the default sort.
+            ViewBag.CategorySort = string.IsNullOrEmpty(sort) ? "category_desc" : string.Empty;
+            ViewBag.ItemCodeSort = sort == "itemcode" ? "itemcode_desc" : "itemcode";
+            ViewBag.NameSort = sort == "name" ? "name_desc" : "name";
+            ViewBag.UnitPriceSort = sort == "unitprice" ? "unitprice_desc" : "unitprice";
+
             var inventoryItems = _applicationDbContext.InventoryItems.Include(i => i.Category);
+
+            switch (sort)
+            {
+                case "category_desc":
+                    inventoryItems = inventoryItems.OrderByDescending(ii => ii.Category.CategoryName)
+                                                   .ThenBy(ii => ii.InventoryItemName);
+                    break;
+
+                case "itemcode":
+                    inventoryItems = inventoryItems.OrderBy(ii => ii.InventoryItemCode);
+                    break;
+
+                case "itemcode_desc":
+                    inventoryItems = inventoryItems.OrderByDescending(ii => ii.InventoryItemCode);
+                    break;
+
+                case "name":
+                    inventoryItems = inventoryItems.OrderBy(ii => ii.InventoryItemName);
+                    break;
+
+                case "name_desc":
+                    inventoryItems = inventoryItems.OrderByDescending(ii => ii.InventoryItemName);
+                    break;
+
+                case "unitprice":
+                    inventoryItems = inventoryItems.OrderBy(ii => ii.UnitPrice)
+                                                   .ThenBy(ii => ii.InventoryItemName);
+                    break;
+
+                case "unitprice_desc":
+                    inventoryItems = inventoryItems.OrderByDescending(ii => ii.UnitPrice)
+                                                   .ThenBy(ii => ii.InventoryItemName);
+                    break;
+
+                default:
+                    inventoryItems = inventoryItems.OrderBy(ii => ii.Category.CategoryName)
+                                                   .ThenBy(ii => ii.InventoryItemName);
+                    break;
+            }
+
             return View(await inventoryItems.ToListAsync());
         }
 
-        // GET: InventoryItems/Details/5
+
         public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             InventoryItem inventoryItem = await _applicationDbContext.InventoryItems.FindAsync(id);
+
             if (inventoryItem == null)
             {
                 return HttpNotFound();
             }
+
             return View(inventoryItem);
         }
 
-        // GET: InventoryItems/Create
+
         public ActionResult Create()
         {
             ViewBag.CategoryId = new SelectList(_applicationDbContext.Categories, "Id", "CategoryName");
+
             return View();
         }
 
-        // POST: InventoryItems/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "InventoryItemId,InventoryItemCode,InventoryItemName,UnitPrice,CategoryId")] InventoryItem inventoryItem)
@@ -56,32 +103,36 @@ namespace MasterDetail.Controllers
             {
                 _applicationDbContext.InventoryItems.Add(inventoryItem);
                 await _applicationDbContext.SaveChangesAsync();
+
                 return RedirectToAction("Index");
             }
 
             ViewBag.CategoryId = new SelectList(_applicationDbContext.Categories, "Id", "CategoryName", inventoryItem.CategoryId);
+
             return View(inventoryItem);
         }
 
-        // GET: InventoryItems/Edit/5
+
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             InventoryItem inventoryItem = await _applicationDbContext.InventoryItems.FindAsync(id);
+
             if (inventoryItem == null)
             {
                 return HttpNotFound();
             }
+
             ViewBag.CategoryId = new SelectList(_applicationDbContext.Categories, "Id", "CategoryName", inventoryItem.CategoryId);
+
             return View(inventoryItem);
         }
 
-        // POST: InventoryItems/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "InventoryItemId,InventoryItemCode,InventoryItemName,UnitPrice,CategoryId")] InventoryItem inventoryItem)
@@ -90,28 +141,34 @@ namespace MasterDetail.Controllers
             {
                 _applicationDbContext.Entry(inventoryItem).State = EntityState.Modified;
                 await _applicationDbContext.SaveChangesAsync();
+
                 return RedirectToAction("Index");
             }
+
             ViewBag.CategoryId = new SelectList(_applicationDbContext.Categories, "Id", "CategoryName", inventoryItem.CategoryId);
+
             return View(inventoryItem);
         }
 
-        // GET: InventoryItems/Delete/5
+
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             InventoryItem inventoryItem = await _applicationDbContext.InventoryItems.FindAsync(id);
+
             if (inventoryItem == null)
             {
                 return HttpNotFound();
             }
+
             return View(inventoryItem);
         }
 
-        // POST: InventoryItems/Delete/5
+
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
@@ -119,8 +176,10 @@ namespace MasterDetail.Controllers
             InventoryItem inventoryItem = await _applicationDbContext.InventoryItems.FindAsync(id);
             _applicationDbContext.InventoryItems.Remove(inventoryItem);
             await _applicationDbContext.SaveChangesAsync();
+
             return RedirectToAction("Index");
         }
+
 
         protected override void Dispose(bool disposing)
         {
