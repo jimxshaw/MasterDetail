4e88b11fc72eece630bf82ca8681117639d0c1fd
Jim Shaw <jimxshaw@gmail.com>
2017-04-16 13:57:36 -0500

    Updated WorkOrder index view.

--- MasterDetail/MasterDetail/Models/ApplicationUser.cs
+++ MasterDetail/MasterDetail/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -24,6 +25,7 @@ namespace MasterDetail.Models
         // explicitly add a WorkOrder collection property in this model class.
         public List<WorkOrder> WorkOrders { get; set; }
 
+        [Display(Name = "Full Name")]
         public string FullName => FirstName + " " + LastName;
 
         public string AddressBlock
