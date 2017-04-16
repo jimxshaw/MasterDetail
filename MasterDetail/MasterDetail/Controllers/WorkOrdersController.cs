374e933e6a55672e19a8e6e56e31dc3a51bf1a26
Jim Shaw <jimxshaw@gmail.com>
2017-04-16 13:35:49 -0500

    Updated models with data annotation attributes for validation. Modified WorkOrder views.

--- MasterDetail/MasterDetail/Controllers/WorkOrdersController.cs
+++ MasterDetail/MasterDetail/Controllers/WorkOrdersController.cs
@@ -9,21 +9,24 @@ using System.Web;
 using System.Web.Mvc;
 using MasterDetail.DataLayer;
 using MasterDetail.Models;
+using Microsoft.AspNet.Identity;
 
 namespace MasterDetail.Controllers
 {
+    [Authorize]
     public class WorkOrdersController : Controller
     {
         private ApplicationDbContext _applicationDbContext = new ApplicationDbContext();
 
-        // GET: WorkOrders
+
         public async Task<ActionResult> Index()
         {
             var workOrders = _applicationDbContext.WorkOrders.Include(w => w.CurrentWorker).Include(w => w.Customer);
+
             return View(await workOrders.ToListAsync());
         }
 
-        // GET: WorkOrders/Details/5
+
         public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
@@ -35,56 +38,59 @@ namespace MasterDetail.Controllers
             {
                 return HttpNotFound();
             }
+
             return View(workOrder);
         }
 
-        // GET: WorkOrders/Create
+
         public ActionResult Create()
         {
             //ViewBag.CurrentWorkerId = new SelectList(_applicationDbContext.ApplicationUsers, "Id", "FirstName");
             ViewBag.CustomerId = new SelectList(_applicationDbContext.Customers, "CustomerId", "AccountNumber");
+
             return View();
         }
 
-        // POST: WorkOrders/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "WorkOrderId,CustomerId,OrderDateTime,TargetDateTime,DropDeadDateTime,Description,WorkOrderStatus,Total,CertificationRequirements,CurrentWorkerId")] WorkOrder workOrder)
         {
             if (ModelState.IsValid)
             {
+                workOrder.CurrentWorkerId = User.Identity.GetUserId();
+
                 _applicationDbContext.WorkOrders.Add(workOrder);
                 await _applicationDbContext.SaveChangesAsync();
-                return RedirectToAction("Index");
+
+                return RedirectToAction("Edit", new { controller = "WorkOrders", action = "Edit", Id = workOrder.WorkOrderId });
             }
 
             //ViewBag.CurrentWorkerId = new SelectList(_applicationDbContext.ApplicationUsers, "Id", "FirstName", workOrder.CurrentWorkerId);
             ViewBag.CustomerId = new SelectList(_applicationDbContext.Customers, "CustomerId", "AccountNumber", workOrder.CustomerId);
+
             return View(workOrder);
         }
 
-        // GET: WorkOrders/Edit/5
+
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             WorkOrder workOrder = await _applicationDbContext.WorkOrders.FindAsync(id);
+
             if (workOrder == null)
             {
                 return HttpNotFound();
             }
-            //ViewBag.CurrentWorkerId = new SelectList(_applicationDbContext.ApplicationUsers, "Id", "FirstName", workOrder.CurrentWorkerId);
-            ViewBag.CustomerId = new SelectList(_applicationDbContext.Customers, "CustomerId", "AccountNumber", workOrder.CustomerId);
+
             return View(workOrder);
         }
 
-        // POST: WorkOrders/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "WorkOrderId,CustomerId,OrderDateTime,TargetDateTime,DropDeadDateTime,Description,WorkOrderStatus,Total,CertificationRequirements,CurrentWorkerId")] WorkOrder workOrder)
@@ -93,14 +99,16 @@ namespace MasterDetail.Controllers
             {
                 _applicationDbContext.Entry(workOrder).State = EntityState.Modified;
                 await _applicationDbContext.SaveChangesAsync();
+
                 return RedirectToAction("Index");
             }
             //ViewBag.CurrentWorkerId = new SelectList(_applicationDbContext.ApplicationUsers, "Id", "FirstName", workOrder.CurrentWorkerId);
             ViewBag.CustomerId = new SelectList(_applicationDbContext.Customers, "CustomerId", "AccountNumber", workOrder.CustomerId);
+
             return View(workOrder);
         }
 
-        // GET: WorkOrders/Delete/5
+
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
@@ -112,10 +120,11 @@ namespace MasterDetail.Controllers
             {
                 return HttpNotFound();
             }
+
             return View(workOrder);
         }
 
-        // POST: WorkOrders/Delete/5
+
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
@@ -123,6 +132,7 @@ namespace MasterDetail.Controllers
             WorkOrder workOrder = await _applicationDbContext.WorkOrders.FindAsync(id);
             _applicationDbContext.WorkOrders.Remove(workOrder);
             await _applicationDbContext.SaveChangesAsync();
+
             return RedirectToAction("Index");
         }
 
