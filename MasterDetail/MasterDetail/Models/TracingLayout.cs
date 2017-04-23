a20d5660f1daefb19eb1a7da8b3ed07f2fad66c3
Jim Shaw <jimxshaw@gmail.com>
2017-04-22 20:13:26 -0500

    Added TracingLayout class to test Log4Net.

---
+++ MasterDetail/MasterDetail/Models/TracingLayout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Core;
+using log4net.Layout;
+
+namespace MasterDetail.Models
+{
+    public class TracingLayout : ExceptionLayout
+    {
+        public override void Format(TextWriter textWriter, LoggingEvent loggingEvent)
+        {
+            base.Format(textWriter, loggingEvent);
+
+            if (loggingEvent.ExceptionObject != null)
+                textWriter.Write(Environment.StackTrace);
+        }
+    }
+
+}
